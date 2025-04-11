15271a5311d082b18afd1dbf637f73a9159e0697
Daniil Varezkin <daniil.varezkin@gmail.com>
2025-04-11 18:36:07 +0300

    Добавлены тесты запросов списка заметок и деталей заметки.

---
+++ Notes.Tests/Notes/Queries/GetNoteDetailsQueryHandlerTests.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Notes.Application.Notes.Queries.GetNoteDetails;
+using Notes.Persistence;
+using Notes.Tests.Common;
+using Shouldly;
+
+namespace Notes.Tests.Notes.Queries
+{
+    [Collection("QueryCollection")]
+    public class GetNoteDetailsQueryHandlerTests
+    {
+        private readonly NotesDbContext Context;
+        private readonly IMapper Mapper;
+
+        public GetNoteDetailsQueryHandlerTests(QueryTestFixture fixture)
+        {
+            Context = fixture.Context;
+            Mapper = fixture.Mapper;
+        }
+
+        [Fact]
+        public async Task GetNoteDetailsQueryHandler_Success()
+        {
+            // Arrange
+            var handler = new GetNoteDetailsQueryHandler(Context, Mapper);
+
+            // Act
+            var result = await handler.Handle(new GetNoteDetailsQuery
+            {
+                UserId = NotesContextFactory.UserBId,
+                Id = Guid.Parse("7156B5D6-54D7-4E3E-972B-A97CA19DE9BC")
+            }, CancellationToken.None);
+
+            // Assert
+            result.ShouldBeOfType<NoteDetailsVm>();
+            result.Title.ShouldBe("Title2");
+            result.Details.ShouldBe("Details2");
+            result.CreationDate.ShouldBe(DateTime.Today);
+
+
+        }
+    }
+}
---
+++ Notes.Tests/Notes/Queries/GetNoteListQueryHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Notes.Application.Notes.Queries.GetNoteList;
+using Notes.Persistence;
+using Notes.Tests.Common;
+using Shouldly;
+using System.Threading.Tasks;
+
+namespace Notes.Tests.Notes.Queries
+{
+    [Collection("QueryCollection")]
+    public class GetNoteListQueryHandlerTests
+    {
+        private readonly NotesDbContext Context;
+        private readonly IMapper Mapper;
+
+        public GetNoteListQueryHandlerTests(QueryTestFixture fixture)
+        {
+            Context = fixture.Context;
+            Mapper = fixture.Mapper;
+        }
+
+        [Fact]
+        public async Task GetNoteListQueryHandler_Success()
+        {
+            // Arrage
+            var handler = new GetNoteListQueryHandler(Mapper, Context);
+
+            // Act
+            var result = await handler.Handle(new GetNoteListQuery
+            {
+                UserId = NotesContextFactory.UserBId
+            }, CancellationToken.None);
+
+            // Assert
+            result.ShouldBeOfType<NoteListVm>();
+            result.Notes.Count.ShouldBe(2);
+        }
+    }
+}
